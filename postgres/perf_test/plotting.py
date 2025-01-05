68f059bbb711336eb5a23fa960d31d7a8bf493d9
tzapadlinski <79110657+tzapadlinski@users.noreply.github.com>
2025-01-05 23:15:51 +0100

    postgres query measuring 1/2

---
+++ postgres/perf_test/plotting.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+
+def plot_result(postgres_result, mongo_result, title):
+    postgres_times, postgres_rows = zip(*postgres_result)
+    mongo_times, mongo_rows = zip(*mongo_result)
+
+    fig, ax = plt.subplots()
+
+    bar_width = 0.35
+    index = range(len(postgres_times))
+
+    bar1 = ax.bar(index, postgres_times, bar_width, label='Postgres')
+    bar2 = ax.bar([i + bar_width for i in index], mongo_times, bar_width, label='MongoDB')
+
+    ax.set_xlabel('Number of Rows')
+    ax.set_ylabel('Time (seconds)')
+    ax.set_title(title)
+    ax.set_xticks([i + bar_width / 2 for i in index])
+    ax.set_xticklabels(postgres_rows)
+    ax.legend()
+
+    plt.savefig(f'result/{title}.png')
+
---
+++ postgres/perf_test/test_query.py
@@ -0,0 +1,15 @@
+
+def test_postgres_queries():
+    pass
+
+def test_insert():
+    pass
+
+def test_select():
+    pass
+
+def test_update():
+    pass
+
+def test_delete():
+    pass
