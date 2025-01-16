9f621eae50e226b7598a8775a5780df6ca796282
tzapadlinski <tomasz1129@gmail.com>
2025-01-16 16:33:43 +0100

    adjust indexes and run tests

--- postgres/scripts/drop_indexes.sql
+++ postgres/scripts/drop_indexes.sql
@@ -1,3 +1,6 @@
+DROP INDEX IF EXISTS idx_calls_call_date;
+DROP INDEX IF EXISTS idx_contacts_calls_call_id;
+DROP INDEX IF EXISTS idx_contacts_calls_contact_id;
+DROP INDEX IF EXISTS idx_contacts_first_name;
+DROP INDEX IF EXISTS idx_contacts_phone_number;
 DROP INDEX IF EXISTS idx_contacts_email;
-DROP INDEX IF EXISTS idx_contacts_last_name;
-DROP INDEX IF EXISTS idx_groups_name;
--- postgres/scripts/indexes.sql
+++ postgres/scripts/indexes.sql
@@ -1,3 +1,6 @@
-CREATE INDEX idx_contacts_email ON Contacts USING hash (email);
-CREATE INDEX idx_contacts_last_name ON Contacts USING hash (last_name);
-CREATE INDEX idx_groups_name ON Groups USING hash (name);
+CREATE INDEX idx_calls_call_date ON calls (call_date);
+CREATE INDEX idx_contacts_calls_call_id ON Contacts_Calls (call_id);
+CREATE INDEX idx_contacts_calls_contact_id ON Contacts_Calls (contact_id);
+CREATE INDEX idx_contacts_first_name ON contacts USING hash (first_name);
+CREATE INDEX idx_contacts_phone_number ON contacts (phone_number);
+CREATE INDEX idx_contacts_email ON contacts (email);
